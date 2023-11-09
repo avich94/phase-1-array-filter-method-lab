f803f600450d8c1c4e9b7064dd639b9a86dc9b45
Austin Vichich <avichich4@gmail.com>
2023-11-09 13:40:30 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(drivers, target) {
+    return drivers.filter(driver => driver.toLowerCase() === target.toLowerCase());
+  }
+
+function fuzzyMatch(drivers, target) {
+    const filteredDrivers = drivers.filter(driver => driver.startsWith(target));
+    return filteredDrivers;
+};
+
+function matchName(drivers, target) {
+   return drivers.filter(driver => driver.name === target);
+};
